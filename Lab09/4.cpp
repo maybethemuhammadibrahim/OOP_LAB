bfadaeed57f8a2c8671dc5a72418e9cbf56f5b8e
Muhammad Ibrahim <mibrahimfiftysix@gmail.com>
2025-04-15 21:46:27 +0500

    Create 4.cpp

---
+++ Lab09/4.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+class PaymentMethod
+{
+protected:
+public:
+    virtual void processPayment(double amount) {}
+};
+
+class CreditCard : public PaymentMethod
+{
+protected:
+    string cardNumber;
+
+public:
+    void processPayment(double amount) override
+    {
+        cout << "Payment of " << amount << " from card " << cardNumber << " has been verified" << endl;
+    }
+};
+
+class DigitalWallet : public PaymentMethod
+{
+protected:
+    double balance;
+
+public:
+    DigitalWallet(double amount = 0) : balance(amount) {}
+
+    void processPayment(double amount) override
+    {
+        if ((balance - amount) > 0)
+        {
+            balance -= amount;
+            cout << "Payment of " << amount << " has been verified and deducted" << endl;
+        }
+        else
+        {
+            cout << "You have insufficient funds to make the payment" << endl;
+        }
+    }
+};
+
+int main()
+{
+    CreditCard c;
+    DigitalWallet d(200);
+    PaymentMethod *p;
+    p = &d;
+    p->processPayment(220);
+    p = &c;
+    p->processPayment(220);
+}
